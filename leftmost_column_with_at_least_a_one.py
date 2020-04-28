8832d1e7a7b06590c71fa15c7224ed6a312c117e
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-28 10:56:54 -0700

    submission leftmost column binary matrix

--- leftmost_column_with_at_least_a_one.py
+++ leftmost_column_with_at_least_a_one.py
@@ -60,3 +60,28 @@ from top right corner. p can only move left or down. If the value at
 p is 0, move down. If the value at p is 1, move left. 
 Try to figure out the correctness and time complexity of this algorithm.
 """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+
+#class BinaryMatrix(object):
+#    def get(self, row: int, col: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        
+      dimensions = binaryMatrix.dimensions()
+      
+      index = -1
+      x = dimensions[0]-1
+      y = dimensions[1]-1
+      
+      while x >= 0 and y >= 0:
+         if binaryMatrix.get(x,y)==1:
+               index = y
+               y -= 1
+               
+         else:
+               x -= 1
+               
+      return index
