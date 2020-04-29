388eff6df73dcf9f1cee7e88fb5b1613f77f5158
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-04-29 11:14:16 -0700

    submission maximal square

--- maximal_square.py
+++ maximal_square.py
@@ -14,6 +14,22 @@ Input:
 
 Output: 4
 """
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        
+        largest = 0
+        
+        if matrix: 
+            rows = len(matrix)
+            cols = len(matrix[0])
+            dp = [[0]*(cols+1) for m in range(rows+1)]
+        
+            for i in range(1, rows+1):
+                for j in range(1, cols+1):
+                    if matrix[i-1][j-1] == '1':
+                        dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
 
-def maximal_square(matrix):
-    pass
+                        if largest < dp[i][j]:
+                            largest = dp[i][j]
+                        
+        return largest * largest
